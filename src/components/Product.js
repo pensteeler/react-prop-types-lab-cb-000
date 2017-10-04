d71f35ef8fcbbcd9338909c1da2edd124fccbd90
John Vascsinec <pensteeler@gmail.com>
2017-10-04 16:18:51 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,47 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 
 export default class Product extends React.Component {
   render() {
     return (
       <div className="product">
-      //<p>{this.props.name}</p>
-      //{this.props.producer ? <small>{this.props.producer}</small> : null}
-      //<p>{this.props.producer}</p>
-      //<p>{this.props.hasWatermark}</p>
-      //<p>{this.props.color}</p>
-      //<p>{this.props.weight}</p>
+        <p>Name: {this.props.name}</p>
+        {this.props.producer ? <small>{this.props.producer}</small> : null}
+        <p>{this.props.hasWatermark ? 'Watermarked' : 'Not watermarked'}</p>
+        <p>Weight: {this.props.weight}</p>
       </div>
     );
   }
 }
 
 Product.defaultProps = {
-  //cone: true,
-  hasWatermark: false
+  hasWatermark: false,
 };
 
 Product.propTypes = {
-  name: PropTypes.string.isRequired,
-  producer: PropTypes.string,
-  hasWatermark: PropTypes.bool,
-  color: PropTypes.oneOf( ['white', 'eggshell-white', 'salmon'] ).isRequired,
+  name: React.PropTypes.string.isRequired,
+  producer: React.PropTypes.string,
+  hasWatermark: React.PropTypes.bool,
+  color: React.PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
   weight: (props, propName) => {
     const weight = props[propName];
-    //PropTypes.number.isRequired
+
     if (weight === undefined) {
-     return new Error('The `weight` prop is required.');
-   }
+      return new Error('The `weight` prop is required.');
+    }
 
-   if (isNaN(weight)) {
-     return new Error('The `weight` prop is not a number.');
-   }
+    if (isNaN(weight)) {
+      return new Error('The `weight` prop is not a number.');
+    }
 
-   const isValidWeight = weight > 80 && weight < 300;
+    const isValidWeight = weight > 80 && weight < 300;
 
-   if (!isValidWeight) {
-     return new Error('The `weight` prop should range between 80 and 300.');
-   }
- },
+    if (!isValidWeight) {
+      return new Error('The `weight` prop should range between 80 and 300.');
+    }
+  },
 };
